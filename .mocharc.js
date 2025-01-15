54486a2a60bea376bf61eafa79be0a9c73bdf12e
Momtchil Momtchev <momtchil@momtchev.com>
2025-01-15 13:16:01 +0100

    Test with Node 23 (#214)
    * test newer node versions
    
    * test newer node versions
    
    * use separate options for Node.js 23
    
    * try moving the configuration to mocha
    
    * show mocha options
    
    * try without dashes
    
    * node-option?
    
    * remove the debug output

---
+++ .mocharc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'spec': 'test/*.test.@(mjs|cjs|js|ts)',
+  'require': [
+    'ts-node/register',
+    'tsconfig-paths/register',
+    './test/force-gc'
+  ],
+  'node-option': +process.versions.node.split('.')[0] >= 23 ? [
+    'no-experimental-strip-types'
+  ] : [],
+  'reporter': 'tap',
+  'timeout': 120000,
+  'v8-expose-gc': true
+};
