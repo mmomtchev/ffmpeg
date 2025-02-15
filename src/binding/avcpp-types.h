ef191edacb5b638bc94ed117d2062182fc74d428
Momtchil Momtchev <momtchil@momtchev.com>
2025-02-15 18:11:42 +0100

    implement the enum trick from SWIG JSE

--- src/binding/avcpp-types.h
+++ src/binding/avcpp-types.h
@@ -41,17 +41,19 @@ public:
   inline av::OptionalErrorCode Get() { return av::throws(); }
   static const int Inputs = 0;
 
-  static const std::string TSType() { return ""; };
+  static std::string TSType() { return ""; };
 };
 
 #define TYPEMAPS_FOR_ENUM(ENUM)                                                                                        \
   template <> class FromJS<ENUM> : public EnumFromJS<ENUM> {                                                           \
   public:                                                                                                              \
     using EnumFromJS<ENUM>::EnumFromJS;                                                                                \
+    static std::string TSType() { return "number & { readonly [__ffmpeg_tag_type]: '" #ENUM "'; };"; }                 \
   };                                                                                                                   \
   template <> class ToJS<ENUM> : public EnumToJS<ENUM> {                                                               \
   public:                                                                                                              \
     using EnumToJS<ENUM>::EnumToJS;                                                                                    \
+    static std::string TSType() { return "number & { readonly [__ffmpeg_tag_type]: '" #ENUM "'; };"; }                 \
   };
 
 // Create typemaps for all basic enums
