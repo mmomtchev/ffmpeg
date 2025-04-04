b1c76e85163eec7f3402bad135d3e23da724b35b
Momtchil Momtchev <momtchil@momtchev.com>
2025-04-04 21:04:48 +0200

    try to address all ffmpeg warnings

--- test/setup.ts
+++ test/setup.ts
@@ -3,6 +3,6 @@ import ffmpeg from '@mmomtchev/ffmpeg';
 exports.mochaHooks = {
   afterEach: global.gc,
   beforeAll: () => {
-    ffmpeg.setLogLevel(process.env.DEBUG_FFMPEG ? ffmpeg.AV_LOG_DEBUG : ffmpeg.AV_LOG_QUIET);
+    ffmpeg.setLogLevel(process.env.DEBUG_FFMPEG ? ffmpeg.AV_LOG_DEBUG : ffmpeg.AV_LOG_WARNING);
   }
 };
